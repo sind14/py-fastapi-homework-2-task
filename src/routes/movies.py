e67530873dbe9464de851806224e0b8fe5e4d42b
Anton Hryhorenko <anton.h.sind@gmail.com>
2025-11-27 06:54:27 +0100

    Update

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -35,10 +35,10 @@ async def get_movies(
     return {
         "movies": movies,
         "prev_page": (
-            f"/api/v1/theater/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
+            f"/theater/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
         ),
         "next_page": (
-            f"/api/v1/theater/movies/?page={page + 1}&per_page={per_page}" if page < total_pages else None
+            f"/theater/movies/?page={page + 1}&per_page={per_page}" if page < total_pages else None
         ),
         "total_pages": total_pages,
         "total_items": total_items,
